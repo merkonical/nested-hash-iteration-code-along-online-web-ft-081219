5e9cc098b575c7c2eea8f3be28f648009f4ce42b
Maverick Adolfo <merkonical@gmail.com>
2019-08-27 08:00:31 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
-      binding.pry 
-  #contact_details_hash.each do |attribute, data|
+      binding.pry contact_details_hash.each do |attribute, data|
       
     end
   end 
